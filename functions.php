17eddb0a382aa23363602a7fe5ae664a371fb1fb
Mikko Paasto <mikko.paasto@ambientia.fi>
2021-12-30 09:21:46 +0200

    Template function for hero. Text between underscores is wrapped span.highlight

--- functions.php
+++ functions.php
@@ -343,3 +343,20 @@ function my_acf_op_init() {
         ));
     }
 }
+
+function spouse_render_main_menu(){
+  get_template_part('partials/after-body-open');
+}
+add_action('spouse_after_body_open', 'spouse_render_main_menu', 10);
+
+
+function get_the_background_image_style( $size = 'full'){
+  if ( has_post_thumbnail()){
+    echo sprintf('style="background-image: url(%s);"', get_the_post_thumbnail_url(get_the_ID(), $size));
+  }
+}
+
+function get_hero_text(){
+  $hero = get_field('hero_text');
+  return preg_replace('/_(.*?)_/', '<span class="highlight">$1</span>', $hero);
+}
