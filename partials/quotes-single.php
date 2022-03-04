4195d93d6343188869c8433afe330491c41732ec
Kimmo Thai <kimmo.thai@ambientia.fi>
2022-03-04 16:28:45 +0200

    WIP semantic reordering, style changes

--- partials/quotes-single.php
+++ partials/quotes-single.php
@@ -1,10 +1,16 @@
 <div class="card col-md-4 col-sm-12">
   <div class="card-body">
-    <p class="quote card-text">
-      <span class="first-quote-mark">“</span>
-      <?php the_sub_field('quote') ?>”
-    </p>
-    <img class="thumbnail" src="<?php the_sub_field('quote_thumbnail')?>" alt="Thumbnail image of <?php the_sub_field('quote_name') ?>">
-    <p class="quote-name"><?php the_sub_field('quote_name') ?></p>
+    <blockquote class="quote card-text">
+      <p class="quote-text">“<?php the_sub_field('quote') ?>”</p>
+      <div class="footer">
+        <?php if (!empty(get_sub_field('quote_thumbnail'))): ?>
+          <img src="<?php the_sub_field('quote_thumbnail') ?>" >
+        <?php else: ?>
+          <!-- TODO FIX IMAGE PATH -->
+          <img src="/wp-content/uploads/2022/03/user.svg" >
+        <?php endif; ?>
+        <span class="author"><?php the_sub_field('quote_name') ?></span>
+      </div>
+    </blockquote>
   </div>
 </div>
