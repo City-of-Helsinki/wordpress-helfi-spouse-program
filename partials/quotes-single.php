1549c395354651c2bf6b436c441397058f0537c7
Kimmo Thai <kimmo.thai@ambientia.fi>
2022-03-09 13:18:02 +0200

    Testimonial styling

--- partials/quotes-single.php
+++ partials/quotes-single.php
@@ -1,16 +1,20 @@
-<div class="card col-md-4 col-sm-12">
-  <div class="card-body">
-    <blockquote class="quote card-text">
-      <p class="quote-text">“<?php the_sub_field('quote') ?>”</p>
-      <div class="footer">
-        <?php if (!empty(get_sub_field('quote_thumbnail'))): ?>
-          <img src="<?php the_sub_field('quote_thumbnail') ?>" >
-        <?php else: ?>
-          <!-- TODO FIX IMAGE PATH -->
-          <img src="/wp-content/uploads/2022/03/user.svg" >
-        <?php endif; ?>
-        <span class="author"><?php the_sub_field('quote_name') ?></span>
-      </div>
-    </blockquote>
+<div class="col-md-4 col-sm-12 testimonial mb-2">
+  <div class="card h-100 mb-2">
+    <div class="card-body">
+      <blockquote class="quote card-text">
+        <p class="quote-text">
+          <?php the_sub_field('quote') ?>”
+        </p>
+        <div class="footer">
+          <?php if (!empty(get_sub_field('quote_thumbnail'))): ?>
+            <img src="<?php the_sub_field('quote_thumbnail') ?>" class="rounded-circle" >
+          <?php else: ?>
+            <!-- TODO FIX IMAGE PATH -->
+            <img class="rounded-circle placeholder" >
+          <?php endif; ?>
+          <span class="author"><?php the_sub_field('quote_name') ?></span>
+        </div>
+      </blockquote>
+    </div>
   </div>
 </div>
--- partials/quotes.php
+++ partials/quotes.php
@@ -1,8 +1,10 @@
 <div class="testimonials">
-  <div class="row flex-row">
-    <?php 
-      while (have_rows('quotes')): the_row(); ?>
-        <?php get_template_part('partials/quotes-single'); ?>
-    <?php endwhile; ?>
+  <div class="grid">
+      <div class="row">
+        <?php 
+          while (have_rows('quotes')): the_row(); ?>
+            <?php get_template_part('partials/quotes-single'); ?>
+        <?php endwhile; ?>
+      </div>
   </div>
 </div>
