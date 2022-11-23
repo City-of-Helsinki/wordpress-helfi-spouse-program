35c396b38e5238f75a5ea5ae0a95bd21bbb3ba46
kimmothai-ambientia <94455211+kimmothai-ambientia@users.noreply.github.com>
2022-11-23 21:33:07 +0200

    Feature/helosk 191 accessibility (#5)
    * Change calendar to upcoming-events, remove unused code
    
    * HELOSK-194 remove target=_blanks
    
    * replace target=_blank
    
    * Contrast changes
    
    * HELOSK-196 - Fieldset tags to be added to forms
    
    * HELOSK-195 - Change social sharing plugin shortcode tag to be used with scriptless social sharing plugin
    
    Co-authored-by: Mikko Paasto <mikko.paasto@ambientia.fi>

--- partials/socials.php
+++ partials/socials.php
@@ -4,7 +4,7 @@
       <div class="row social-icon-bar">
           <div class="col-6 mx-auto text-center my-4">
             <span class="social-title"><?php dynamic_sidebar( 'social_title' ); ?></span>
-            <?php echo do_shortcode('[SHARING_PLUS]'); ?>
+            <?php echo do_shortcode('[scriptless]'); ?>
           </div>
       </div>
   <?php endif; ?>
