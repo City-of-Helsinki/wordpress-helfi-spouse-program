aab465e37a31b2cbaa5c6c2ab48c31aa04ef1271
Antti Koskinen <koskinenaa@users.noreply.github.com>
2024-05-23 18:14:59 +0300

    Create config.php

---
+++ functions/static-pages/config.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Spouse;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	die();
+}
+
+function static_pages(): array {
+	return array(
+		'login' => array(
+			'slug' => 'login',
+			'title' => __( 'Sign In', 'spouse' ),
+			'content' => ''
+		),
+		'register' => array(
+			'slug' => 'register',
+			'title' => __( 'Sign up', 'spouse' ),
+			'content' => ''
+		),
+		'account' => array(
+			'slug' => 'member-account',
+			'title' => __( 'Your Account', 'spouse' ),
+			'content' => ''
+		),
+		'password-lost' => array(
+			'slug' => 'password-lost',
+			'title' => __( 'Forgot Your Password?', 'spouse' ),
+			'content' => ''
+		),
+		'password-reset' => array(
+			'slug' => 'member-password-reset',
+			'title' => __( 'Pick a New Password', 'spouse' ),
+			'content' => ''
+		),
+		'main-page' => array(
+			'slug' => 'main-page',
+			'title' => __( 'Welcome', 'spouse' ),
+			'content' => ''
+		)
+	);
+}
