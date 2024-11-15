571db84a2ac753d4a8e80576206dbe8c4db47177
Kimmo Thai <kimmo.thai@hiq.fi>
2024-11-15 17:11:58 +0200

    Add click to remove the element

--- partials/notification.php
+++ partials/notification.php
@@ -5,14 +5,18 @@ $notification_body = get_theme_mod('notification_body');
 $notification_visibility = get_theme_mod('notification_visibility');
 ?>
 
-<?php if (true == $notification_enabled): ?>
-<div class="container-fluid sp-notification">
-    <div class="sp-notification-title">
-        <?php echo $notification_title; ?>
-        <!-- <?php //echo $notice; ?> -->
+<?php if ( true == $notification_enabled ): ?>
+<div class="alert alert-dismissible fade show container-fluid sp-notifications bg-info rounded-0" role="alert">
+    <div class="sp-notification">
+        <div class="sp-notification-title">
+            <?php echo $notification_title; ?>
+        </div>
+        <div class="sp-notification-body">
+            <?php echo $notification_body; ?>
+        </div>
     </div>
-    <div class="sp-notification-body">
-        <?php echo $notification_body; ?>
-    </div>
+    <button type="button" class="close" data-dismiss="alert" aria-label="<?php echo __('Close', 'spouse' ); ?>">
+        <span aria-hidden="true">x</span>
+    </button>
 </div>
 <?php endif; ?>
