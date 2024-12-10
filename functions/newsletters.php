6d8a976a73e5c00af09991b986afb06563f708d0
Kimmo Thai <kimmo.thai@hiq.fi>
2024-12-10 17:32:19 +0200

    HTML and styles

--- functions/newsletters.php
+++ functions/newsletters.php
@@ -30,22 +30,35 @@ function spouse_get_newsletters() {
 
 function spouse_print_newsletters($newsletters) {
     ?>
-    <div class="d-flex newsletters-wrapper">
+    <div class="d-flex newsletters mt-3">
         <?php
-
         if ( is_array($newsletters) ) {
             foreach( $newsletters as $newsletter ) {
                 $newsletter_title = $newsletter->post_title;
-                $publish_date = $newsletter->post_date;
-                $featured_image = get_the_post_thumbnail($newsletter->ID);
+                $publish = strtotime( $newsletter->post_date );
+                $publish_date = ( new \DateTime() )->setTimestamp( $publish )->format( 'j.m.Y' );
+                $featured_image = get_the_post_thumbnail_url($newsletter->ID);
                 $pdf = wp_get_attachment_url(get_post_meta($newsletter->ID, 'newsletter_pdf', true));
+                $placeholder_image = get_field('placeholder_image', 'options_activity_setttings');
                 ?>
-                <div class="newsletter-column">
-
-                    <?php echo $newsletter_title; ?>
-                    <?php echo $publish_date; ?>
-                    <!-- <?php echo $featured_image; ?> -->
-                    <?php echo $pdf; ?>
+                <div class="newsletters-column my-3">
+                  <div class="newsletter clearfix">
+                    <a href="<?php echo $pdf; ?>">
+                      <div class="newsletter-content-wrap card border-0 flex-fill">
+                        <div class="newsletter-content">
+                          <?php if( !empty($featured_image) ): ?>
+                            <div class="newsletter-featured-image" style="background-image: url(<?php echo $featured_image; ?>);"></div>
+                          <?php else: ?>
+                            <div class="newsletter-no-image" style="background-image: url(<?php echo $placeholder_image; ?>);"></div>
+                          <?php endif; ?>
+                          <div class="text-content card-body">
+                            <h3 class="post-title"><?php echo $newsletter_title; ?></h3>
+                            <p class="publish-date"><?php echo $publish_date; ?></p>
+                          </div>
+                        </div>
+                      </div>
+                    </a>
+                  </div>
                 </div>
                 <?php
             }
