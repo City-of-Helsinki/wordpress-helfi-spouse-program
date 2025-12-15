48fcd052c82c1eea638ae854fdaa6821f5cf5a42
Didzis Zvaigzne <didzis.zvaigzne@hiq.fi>
2025-12-15 12:42:59 +0200

    HELIHHSP-73 Remove comments

--- header.php
+++ header.php
@@ -7,9 +7,6 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" >
     <link rel="profile" href="https://gmpg.org/xfn/11">
-<!--    <link href="https://fonts.googleapis.com/css2?family=Sora:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">-->
-<!--    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">-->
-<!--    <link href="https://fonts.googleapis.com/css2?family=Lexend:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet">-->
     <?php wp_head(); ?>
 </head>
 
